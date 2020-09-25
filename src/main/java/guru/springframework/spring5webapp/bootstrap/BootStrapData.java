07df9484a18fc458a39e2729c6d716aa612ec660
Marcelo Scarpim <marcelo.scarpim@monitoratec.com.br>
2020-09-25 10:54:57 -0300

    Add Publisher entity and relationship

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,32 +14,50 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher publisher1 = new Publisher("Jonh Doe", "Rua 1", "Dois Córregos", "SP", "17300000");
+        publisherRepository.save(publisher1);
+
+        System.out.println("Publisher count: " + publisherRepository.count());
+
         Author author1 = new Author("Robert", "C. Martin");
         Book book1 = new Book("Clean Code", "3546545654");
         author1.getBooks().add(book1);
         book1.getAuthors().add(author1);
 
+        book1.setPublisher(publisher1);
+        publisher1.getBooks().add(book1);
+
         authorRepository.save(author1);
         bookRepository.save(book1);
+        publisherRepository.save(publisher1);
 
         Author author2 = new Author("Tiago", "Brunet");
         Book book2 = new Book("Especialista em Pessoas", "9788542218756");
         author2.getBooks().add(book2);
         book2.getAuthors().add(author2);
 
+        book2.setPublisher(publisher1);
+        publisher1.getBooks().add(book2);
+
         authorRepository.save(author2);
         bookRepository.save(book2);
+        publisherRepository.save(publisher1);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher books count: " + publisher1.getBooks().size());
+
+
 
     }
 
